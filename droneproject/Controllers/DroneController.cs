bc4d60e8ed69995118b4aa13bec9da07c4ef9bda
Nnabueze Opara <villavelle101@gmail.com>
2022-02-13 02:49:53 +0100

    installed packages for swagger UI

---
+++ droneproject/Controllers/DroneController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace droneproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DroneController : ControllerBase
+    {
+
+        /// <summary>
+        /// Create drone
+        /// </summary>
+        [HttpPost]
+        [Route("CreateDrone")]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+    }
+}
